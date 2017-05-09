f9d1a8aa051f5ed2f5ff5ea1a0f5f141861fd26e
mothermoose <melanie.ilseman@gmail.com>
2017-05-08 21:36:33 -0500

    added destroy action and banners to denote changes (dismisable

--- app/controllers/oils_controller.rb
+++ app/controllers/oils_controller.rb
@@ -15,10 +15,37 @@ class OilsController < ApplicationController
                   image:params[:image]
                    )
     oil.save
+    flash[:success] = "Oil Created"
+    redirect_to "/oils/#{ oil.id }"
   end
 
   def show
     oil_id = params[:id]
     @oil = Oil.find_by(id:oil_id)
-  end
+  end 
+
+  def edit
+    @oil = Oil.find(params[:id])
+  end 
+
+  def update
+    oil = Oil.find(params[:id])
+    oil.assign_attributes(
+                        name: params[:name],
+                        uses: params[:uses],
+                        price: params[:price],
+                        image: params[:image]
+                        )
+    oil.save
+    flash[:success] = "Oil Updated"
+    redirect_to "/oils/#{ oil.id }"
+  end 
+
+  def destroy
+    oil = Oil.find(params[:id])
+    oil.destroy
+    flash[:warning] = "Oil Destroyed"
+    redirect_to "/"
+  end 
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
 
   get '/oils/new' => 'oils#new'
   post '/oils' => 'oils#create'
-  
+
   get '/oils/:id' => 'oils#show'
+
+  get '/oils/:id/edit' => 'oils#edit'
+  patch 'oils/:id' => 'oils#update'
+
+  delete '/oils/:id' => 'oils#destroy'
 end
