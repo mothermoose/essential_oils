9bd66374593a3e4a3c096d3708ba8d90a3f6e31d
mothermoose <melanie.ilseman@gmail.com>
2017-05-10 17:34:48 -0500

    added tax

--- app/models/oil.rb
+++ app/models/oil.rb
@@ -4,6 +4,14 @@ class Oil < ApplicationRecord
     message = "Discount Item!" if price <= 2
     message = "Everyday Value!" if price > 2
     message
+  end  
+
+  def tax
+    price * 0.09
   end 
+
+  def total
+    price + tax
+  end
 
 end
